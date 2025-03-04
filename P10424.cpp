98bfd31797fa348f02796cad751453808b5c6e5d
taizhen <tzhch>
2025-03-04 10:35:29 -0500

    P10424

--- P10424.cpp
+++ P10424.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <string>
+using namespace std;
 
-int goodnum(int n) {
-    
+
+//other to solve more clean and efficent
+bool goodnum(int n) {
+  for(int i=1; n; i++, n/=10) {
+    if(n%2 != i%2) return false;
+  }
+  return true;
 }
 
 
-int main() {
 
+
+/*
+bool goodnum(int n) {
+  for(int i=1; n>0; i++) {
+    int check = n;
+    check%=10;
+    if(check%2 != i%2) return false;
+    n/=10;
+  }
+  return true;
+}
+*/
+
+int main() {
+  int N;
+  cin >> N;
+  int cnt = 0;
+  for(int i=1; i<=N; i++) {
+    if(goodnum(i)) cnt++;
+  }
+
+  cout << cnt << endl;
+  return 0;
 }
