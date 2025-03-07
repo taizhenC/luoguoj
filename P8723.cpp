1622b34925b44832ae72a3a15d6e4f5f264667d4
taizhen <tzhch>
2025-03-06 22:53:08 -0500

    P8723

---
+++ P8723.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <format>
+using namespace std;
+
+void scale(int x) {
+  for(int i=1; i<x; i++) {
+    for(int j=i; j<x; j++) {
+      
+    }
+  }
+}
+
+int main() {
+  int P;
+  cin >> P;
+
+}
