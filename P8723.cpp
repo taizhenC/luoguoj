81251cd6ee7f9ff711d40843bb191a6f58a49d60
taizhen <tzhch>
2025-03-11 18:06:09 -0400

    P8723

--- P8723.cpp
+++ P8723.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <format>
+#include <string>
+#include <vector>
 using namespace std;
+
+string trans(int x, int y, int num) {
+  int target = x*y;
+  string final;
+  while(target) {
+    int reminder = target%num;
+    final += (reminder < 10 ? reminder + '0' : reminder - 10 + 'A');
+    target/=num;
+  }
+  string res;
+  for(int i = final.length() -1; i>=0; i--) {
+    res += final[i];
+  }
+
+  return res;
+}
+
 
 void scale(int x) {
   for(int i=1; i<x; i++) {
-    for(int j=i; j<x; j++) {
-      
+    for(int j=1; j<=i; j++) {
+
+      cout << format("{}*{}={}",trans(i,1,x),trans(j,1,x),trans(i, j, x)) << " ";
+      // if(j = i) {
+      //   cout << "\n";
+      // }
     }
+    cout << endl;
   }
 }
 
@@ -14,4 +38,6 @@ int main() {
   int P;
   cin >> P;
 
+  scale(P);
+  return 0;
 }
