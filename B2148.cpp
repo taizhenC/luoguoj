5369614361e9d8ef3cb4fdb910f522bc39b4b12d
taizhen <tzhcheung@gmail.com>
2025-03-30 19:13:39 -0400

    second

--- B2148.cpp
+++ B2148.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 double fx(double x, int n) {
-    if(n == 1) return x/(1+x);
-    return x / (n+fx(x , n-1));
+  if(n == 1) return x/(1+x);
+  return x / (n+fx(x, n-1));
 }
 
-
 int main() {
-    double x;
-    int n;
-    cin >> x >> n;
-    cout.precision(2);
-    cout << fixed;
-    cout << fx(x ,n) << endl;
+  double x;
+  int n; 
+  cin >> x >> n;
+  cout.precision(2);
+  cout << fixed;
+  cout << fx(x ,n) << endl;
 }
