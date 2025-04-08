14c5c40281c02bc1a473085d46142ef5ba42099e
taichicken <tzhcheung@gmail.com>
2025-04-08 18:17:25 -0400

    P5461

--- P5461.cpp
+++ P5461.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 using namespace std;
 
-void matrix(int n, auto& square) {
-  if(n == 0) {
-    for(auto x: square) cout << x << " ";
-  } else {
-    for(int i = 0; i < 2^n-1; i++) {
-      for(int j = 0; j < 2^n-1; j++) {
-        square[i][j] = 0;
-      }
-    }
-    
-    return()
 
+void matrix(int x, int y, int size, auto& square) {
+  if(size == 1) return;
+
+  int half = size/2;
+  for(int i = y; i < half + y; i++) {
+    for(int j = x; j < half + x; j++) {
+      square[i][j] = 0;
+    }
   }
+  matrix(x + half, y, half, square);
+  matrix(x, y + half, half, square);
+  matrix(x + half, y + half, half, square);
+  
 }
 
 int main() {
   int n;
   cin >> n;
-  
-  auto square = vector(2^n, vector(2^n,1));
-  matrix(n, square);
+  int size = pow(2, n);
+  auto square = vector(size, vector(size,1));
+  matrix(0, 0, size, square);
+
+  for(auto& x : square) {
+    for(auto& y : x) {
+      cout << y << " ";
+    }
+    cout << endl;
+  }
 }
