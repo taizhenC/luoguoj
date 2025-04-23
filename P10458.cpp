2ad0a9cf7f51ccac33d75f743aba3526e61d41eb
taichicken <tzhcheung@gmail.com>
2025-04-23 19:03:56 -0400

    P10458

--- P10458.cpp
+++ P10458.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 #include <vector>
-
-void fractal(int n) {
-  
+#include <math.h>
+#include <string>
+void fractal(auto& matrix, int x, int y, int n) {
+  if(n == 1)
+  {
+    matrix[x][y] = 'X';
+    return;
+  }
+  int m1 = n / 3;
+  int m2 = m1*2;
+  fractal(matrix, x, y, m1);
+  fractal(matrix, x, y + m2, m1);
+  fractal(matrix, x + m1, y + m1, m1);
+  fractal(matrix, x + m2, y, m1);
+  fractal(matrix, x + m2, y + m2, m1);
 }
 
 int main() {
   int n;
-  while(std::cin >> n) {
-    if (n == -1) break;
-    fractal(n);
+  while(std::cin >> n)
+  {
+    if(n == -1) break;
+    int m = pow(3, n - 1);
+    std::vector<std::string> matrix(m, std::string(m, ' '));
+    fractal(matrix, 0, 0, m);
+    for(auto &x : matrix)
+    {
+      std::cout << x << std::endl;
+    }
+    std::cout << "-" << std::endl;
   }
+  
 }
