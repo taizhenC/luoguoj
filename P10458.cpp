5e74d9087b63bc79485e68aed07dc60acb51fb2d
taizhen <tzhcheung@gmail.com>
2025-04-23 19:30:56 -0400

    P10458 2

--- P10458.cpp
+++ P10458.cpp
@@ -2,34 +2,39 @@
 #include <vector>
 #include <math.h>
 #include <string>
-void fractal(auto& matrix, int x, int y, int n) {
+using namespace std;
+
+void fractal(auto& matrix, int x, int y, int n)
+{
   if(n == 1)
   {
     matrix[x][y] = 'X';
     return;
   }
-  int m1 = n / 3;
-  int m2 = m1*2;
-  fractal(matrix, x, y, m1);
-  fractal(matrix, x, y + m2, m1);
-  fractal(matrix, x + m1, y + m1, m1);
-  fractal(matrix, x + m2, y, m1);
-  fractal(matrix, x + m2, y + m2, m1);
+  int coor1 = n/3;
+  int coor2 = coor1 * 2;
+
+  fractal(matrix, x, y, coor1);
+  fractal(matrix, x, y + coor2, coor1);
+  fractal(matrix, x + coor1, y + coor1, coor1);
+  fractal(matrix, x + coor2, y, coor1);
+  fractal(matrix, x + coor2, y + coor2, coor1);
 }
 
-int main() {
+int main()
+{
   int n;
-  while(std::cin >> n)
+  while(cin >> n)
   {
     if(n == -1) break;
     int m = pow(3, n - 1);
-    std::vector<std::string> matrix(m, std::string(m, ' '));
+    vector<string> matrix(m, string(m, ' '));
     fractal(matrix, 0, 0, m);
-    for(auto &x : matrix)
-    {
-      std::cout << x << std::endl;
+    for(auto &x: matrix) {
+      cout << x << endl;
     }
-    std::cout << "-" << std::endl;
+    cout << "-" << endl;
   }
-  
+
+  return 0;
 }
