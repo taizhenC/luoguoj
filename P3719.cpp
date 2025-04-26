dde862752b60aef3724a5145874302f8ca36496f
taizhen <tzhcheung@gmail.com>
2025-04-26 12:08:57 -0400

    P3719

--- P3719.cpp
+++ P3719.cpp
@@ -5,6 +5,65 @@
 using namespace std;
 
 
+//four version
+int rexp()
+{
+  /*
+  same idea as the version three, but do a little modfication,
+  we dont need the parameter pass to the function,
+  instead we input the value to the fuction, code become more clean,
+  but become more abstract as the previous version.
+  */
+  char s;
+  int cnt = 0;
+  while(cin >> s)
+  {
+    if(s == 'a') cnt++;
+    if(s == '(') cnt +=rexp();
+    if(s == ')') return cnt;
+    if(s == '|') return max(cnt , rexp());
+  }
+  return cnt;
+}
+
+
+//three version
+int rexp(string &s, int &i)
+{
+  /*
+  1. start from index i, tile next complete string, 
+  find it maximum length
+  2. if the string have not () and |, return maximum in cnt
+  3. if we have () we only recursive the center, () -> center,
+  if we have |, we only recursive the right term, | -> right
+  */
+  int cnt = 0;
+  for(; i < s.length();i++)
+  {
+    if(s[i] == 'a')
+    {
+      cnt++;
+    }
+    else if(s[i] == '(')
+    {
+      cnt += rexp(s, ++i);
+    }
+    else if(s[i] == ')')
+    {
+      return cnt;
+    }
+    else if (s[i] == '|')
+    {
+      return max(cnt, rexp(s, ++i));
+    }
+  }
+
+  return cnt;
+}
+
+
+//second version
+//l mean the leftest pos, r mean righest pos.
 int rexp(string &s, int l, int r)
 {
  /*
@@ -100,7 +159,8 @@ int rexp(string s)
 
 int main()
 {
-  string s;
-  cin >> s;
-  cout << rexp(s, 0, s.length()-1) << endl;
+  // string s;
+  // cin >> s;
+  // int i = 0;
+  cout << rexp() << endl;
 }
